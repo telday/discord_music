7e524f8be6855f6508cea329ba23c0e39725a181
ewright393 <exw4938@g.rit.edu>
2018-03-07 18:53:41 -0500

    Moved music capabilities to Playlist Class and renamed main file

---
+++ bot/Playlist.py
@@ -0,0 +1,49 @@
+"""
+	file: Playlist.py
+	description: class for a playlist for the music bot to use
+	
+	author: Ellis Wright
+	language: python3.6
+"""
+import discord
+
+
+class Playlist:
+	#List of all the libraries opus needs
+	OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
+	player = None
+	voice_channel = None
+	
+	def __init__(self):
+		self.load_opus_lib()
+	
+	
+	def load_opus_lib(opus_libs=OPUS_LIBS):
+		"""
+			Loads the opus library for the bot to used. Required for sound.
+		"""
+		if discord.opus.is_loaded():
+			return True
+
+		for opus_lib in opus_libs:
+			try:
+				discord.opus.load_opus(opus_lib)
+				return
+			except OSError:
+				pass
+
+	
+	async def play_link(self, bot, voice_channel, url):
+		"""
+			Plays the given link on the given bot in the given voice channel.
+		"""
+		if self.player != None and not self.player.is_done():
+			self.player.stop()
+		
+		try:
+			self.voice_channel = await bot.join_voice_channel(voice_channel)
+		except Exception:
+			pass
+		
+		self.player = await self.voice_channel.create_ytdl_player(url)
+		self.player.start()
--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,5 @@
 """
-	file: d_bot.py
+	file: bot.py
 	author: Ellis Wright/Brian Shaw
 	language: python 3.6
 	description: Bot with few functionalities for discord
@@ -10,28 +10,11 @@ import discord
 from time import time
 from random import choice
 from discord.ext import commands
-import simple_commands
-from discord import opus
+from Playlist import Playlist
 
 bot = commands.Bot(command_prefix=":", description="Test Bot")
 start_time = 0
-vcg = None
 plr = None
-
-
-OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
-
-
-def load_opus_lib(opus_libs=OPUS_LIBS):
-	if opus.is_loaded():
-		return True
-
-	for opus_lib in opus_libs:
-		try:
-			opus.load_opus(opus_lib)
-			return
-		except OSError:
-			pass
 
 
 @bot.event
@@ -40,9 +23,10 @@ async def on_ready():
 		Called on script start
 	"""
 	global start_time
+	global plr
 	print("Logged in as: ", bot.user.name)
 	start_time = time()
-	load_opus_lib()
+	plr = Playlist()
 
 #NOTE: Only works on messages cached after the bot starts running
 @bot.event
@@ -93,27 +77,12 @@ async def yt(ctx, url : str):
 	"""
 		Takes in a youtube link and plays the video
 	"""
-	global vcg
 	global plr
-	if plr != None and not plr.is_done():
-		await bot.say("The bot is currently playing a video... Use command :kill to stop the current video.")
-		return
+	#Get the voice channel the author who sent the command is in
 	author = ctx.message.author
 	voice_channel = author.voice_channel
-	if vcg == None:
-		vcg = await bot.join_voice_channel(voice_channel)
 	
-	plr = await vcg.create_ytdl_player(url)
-	plr.start()
-	
-@bot.command(pass_context=True, description="Kills the current players song")
-async def kill():
-	"""
-		Kills the current song playing in the player.
-	"""
-	global plr
-	if plr != None:
-		plr.stop()
+	await plr.play_link(bot, voice_channel, url)
 
 
 bot.run('Mzc1Njg5Mjg1NjkwNTg5MTg2.DNzfkw.SaEXRWDODM5NaeBh0sOnxy6j6ok')
