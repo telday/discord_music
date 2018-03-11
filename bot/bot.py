b4d1052efd2e5890d70e1835d609a357034b2551
ewright393 <exw4938@g.rit.edu>
2018-03-10 23:48:11 -0500

    Started rewrite of main sound class

--- bot/bot.py
+++ bot/bot.py
@@ -7,63 +7,90 @@
 #NOTE: DEPENDENCY: python3 discord module
 #https://discordapp.com/api/oauth2/authorize?client_id=375689285690589186&scope=bot&permissions=0
 import discord
-from time import time
-from random import choice
 from discord.ext import commands
-from Playlist import Playlist
+from queue import Queue
+from threading import Thread, Lock, Condition
 
 bot = commands.Bot(command_prefix=":", description="Test Bot")
-start_time = 0
-plr = None
+lock = Lock()
+condition = Condition(lock)
 
+class Player(Thread):
+	voice_state = None
+	
+	def __init__(self, bot):
+		Thread.__init__(self, daemon=True)
+		self.bot = bot
+		self.queue = Queue()
+	
+	async def join_channel(self, channel):
+		self.voice_state =  await bot.join_voice_channel(channel)
+		
+	async def create_player(self, url):
+		player = await self.voice_state.create_ytdl_player(url)
+		player.start()
+	
+	
+	def run(self):
+		global condition
+		while True:
+			if self.queue.empty():
+				condition.acquire()
+				condition.wait()
+				condition.release()
+				
+			info = self.queue.get()
+			try:
+				self.join_channel(info[0])
+			except Exception:
+				pass
+			
+			self.create_player(info[1])
+		
+		
+		
+class Playlist:
+	OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
+	voice_channel = None
+
+	def __init__(self, bot):
+		self.load_opus_lib()
+		self.bot = bot
+		self.player = Player(self.bot)
+		self.player.start()
+
+	def load_opus_lib(opus_libs=OPUS_LIBS):
+		"""
+			Loads the opus library for the bot to used. Required for sound.
+		"""
+		if discord.opus.is_loaded():
+			return True
+
+		for opus_lib in opus_libs:
+			try:
+				discord.opus.load_opus(opus_lib)
+				return
+			except OSError:
+				pass
+
+	@commands.command(pass_context=True)
+	async def add(self, ctx, url : str):
+		global condition
+		info = (ctx.message.author.voice_channel, url)
+		self.player.queue.put(info)
+		
+		condition.acquire()
+		condition.notify()
+		condition.release()
+		
 
 @bot.event
 async def on_ready():
 	"""
 		Called on script start
 	"""
-	global start_time
-	global plr
 	print("Logged in as: ", bot.user.name)
-	start_time = time()
 
-#NOTE: Only works on messages cached after the bot starts running
-@bot.event
-async def on_message_delete(message):
-	"""
-		String
-		Takes in the message object, uses a formatter to grab text
-		and prints to the chat.
-		Note: The author and deletor can also be derived from the message obj
-	"""
-	#Corrects for infinitely recursing bot messages
-	if message.author.name == "test-bot":
-		return
-	fmt = 'Why would you delete a message as beautiful as:\n{0.content}'
-	await bot.send_message(message.channel, fmt.format(message))
-
-
-@bot.command(description="Flips a coin?")
-async def flip_coin():
-	"""
-		Really?
-	"""
-	await bot.say(choice(["Heads", "Tails"]))
-
-
-@bot.command(description="Rolls a die")
-async def roll_die(minimum : int, maximum : int):
-	"""
-		NatNum * NatNum -> NatNum
-		Takes in an integer as the min and max number on the die and prints a
-		random selection to the chat.
-	"""
-	await bot.say(simple_commands.roll_die_l(minimum, maximum))
-
-@bot.command(description="Gets the uptime of the bot")
-async def uptime():
-	global start_time
-	await bot.say("{0:.2f}".format(time() - start_time) + " seconds")
 
 @bot.command(pass_context=True, description="Play a yt link")
 async def yt(ctx, url : str):
@@ -85,6 +112,6 @@ async def v(vol : float):
 	else:
 		print("what the fuck")
 
-plr = Playlist(bot)
-bot.add_cog(plr)
+
+bot.add_cog(Playlist(bot))
 bot.run('Mzc1Njg5Mjg1NjkwNTg5MTg2.DNzfkw.SaEXRWDODM5NaeBh0sOnxy6j6ok')
