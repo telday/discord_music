ca140717abd481c97887202d6bd7ce0f6362b0f0
ewright393 <exw4938@g.rit.edu>
2018-04-04 12:27:02 -0400

    Updated bot to play music on command

--- bot/bot.py
+++ bot/bot.py
@@ -7,57 +7,26 @@
 #NOTE: DEPENDENCY: python3 discord module
 #https://discordapp.com/api/oauth2/authorize?client_id=375689285690589186&scope=bot&permissions=0
 import discord
+import asyncio
 from discord.ext import commands
 from queue import Queue
-from threading import Thread, Lock, Condition
+import string
 
-bot = commands.Bot(command_prefix=":", description="Test Bot")
-lock = Lock()
-condition = Condition(lock)
+bot = commands.Bot(command_prefix="$", description="Test Bot")		
 
-class Player(Thread):
+
+class Playlist:
+	OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
+
+	player = None
 	voice_state = None
 	
 	def __init__(self, bot):
-		Thread.__init__(self, daemon=True)
-		self.bot = bot
-		self.queue = Queue()
-	
-	async def join_channel(self, channel):
-		self.voice_state =  await bot.join_voice_channel(channel)
-		
-	async def create_player(self, url):
-		player = await self.voice_state.create_ytdl_player(url)
-		player.start()
-	
-	
-	def run(self):
-		global condition
-		while True:
-			if self.queue.empty():
-				condition.acquire()
-				condition.wait()
-				condition.release()
-				
-			info = self.queue.get()
-			try:
-				self.join_channel(info[0])
-			except Exception:
-				pass
-			
-			self.create_player(info[1])
-		
-		
-		
-class Playlist:
-	OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
-	voice_channel = None
-
-	def __init__(self, bot):
 		self.load_opus_lib()
 		self.bot = bot
-		self.player = Player(self.bot)
-		self.player.start()
+		self.queue = asyncio.Queue()
+		self.play_event = asyncio.Event()
+		self.bot.loop.create_task(self.play())
 
 	def load_opus_lib(opus_libs=OPUS_LIBS):
 		"""
@@ -73,36 +42,55 @@ class Playlist:
 			except OSError:
 				pass
 
+	def toggle(self):
+		self.bot.loop.call_soon_threadsafe(self.play_event.set)
+	
+	songs = ["https://www.youtube.com/watch?v=CD-E-LDc384", "https://www.youtube.com/watch?v=94bGzWyHbu0", "https://www.youtube.com/watch?v=zUzd9KyIDrM", "https://www.youtube.com/watch?v=3L4YrGaR8E4"]
+	@commands.command(pass_context=True)
+	async def add_song_remote(self, ctx):
+		if self.voice_state == None:
+			self.voice_state = await self.bot.join_voice_channel(ctx.message.author.voice_channel)
+		for i in self.songs:
+			info = (ctx.message.author.voice_channel, i)
+			await self.queue.put(info)
+	
 	@commands.command(pass_context=True)
 	async def add(self, ctx, url : str):
-		global condition
 		info = (ctx.message.author.voice_channel, url)
-		self.player.queue.put(info)
-		
-		condition.acquire()
-		condition.notify()
-		condition.release()
-		
+		if self.voice_state == None:
+			self.voice_state = await self.bot.join_voice_channel(info[0])
+		print("Adding song to playlist on voice channel {} with url: {}".format(ctx.message.author.voice_channel, url))
+		await self.queue.put(info)
 
+	@commands.command()
+	async def kill(self):
+		if self.player != None:
+			self.player.stop()
+	
+	async def play(self):
+		while True:
+			self.play_event.clear()
+			info = await self.queue.get()
+			self.player = await self.voice_state.create_ytdl_player(info[1], after=self.toggle)
+			self.player.start()
+			await self.play_event.wait()
+
+	@commands.command()
+	async def pause(self):
+		if self.player != None:
+			self.player.pause()
+	
+	@commands.command()
+	async def resume(self):
+		if self.player != None:
+			self.player.resume()
+	
 @bot.event
 async def on_ready():
 	"""
 		Called on script start
 	"""
 	print("Logged in as: ", bot.user.name)
-
-
-@bot.command(pass_context=True, description="Play a yt link")
-async def yt(ctx, url : str):
-	"""
-		Takes in a youtube link and plays the video
-	"""
-	global plr
-	#Get the voice channel the author who sent the command is in
-	author = ctx.message.author
-	voice_channel = author.voice_channel
-
-	await plr.play_link(bot, voice_channel, url)
 
 @bot.command()
 async def v(vol : float):
@@ -112,6 +100,9 @@ async def v(vol : float):
 	else:
 		print("what the fuck")
 
+@bot.command()
+async def scared():
+	await bot.say("https://i.imgur.com/Dqbyu3x.gifv")
 
 bot.add_cog(Playlist(bot))
-bot.run('Mzc1Njg5Mjg1NjkwNTg5MTg2.DNzfkw.SaEXRWDODM5NaeBh0sOnxy6j6ok')
+bot.run('NDI2NTI0MDg0MDA0NzE2NTU3.DZXPBA.h7B44TMwKXmD-PWbgF5_LK8bZj4')
