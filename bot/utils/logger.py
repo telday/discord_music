fb3d9f864c6bf57379b4f20e52d2ebfbd3979d24
ewright393 <exw4938@g.rit.edu>
2018-04-08 19:58:12 -0400

    Added comments

--- bot/utils/logger.py
+++ bot/utils/logger.py
@@ -1,3 +1,10 @@
+"""
+	file: logger.py
+	author: Ellis Wright
+	language: python3.6
+	description: Simple threaded logging application to log all messages
+	sent in the server
+"""
 from threading import Thread
 from time import strftime
 from queue import Queue
@@ -10,12 +17,22 @@ class Logger(Thread):
 		self.is_logging = True
 	
 	def log_event(self, event:str):
+		"""
+			Adds an event to the logger's queue
+		"""
+		#TODO Add capabilities for logging file/media uploads
 		self.queue.put(event)
 	
 	def stop_logging(self):
-		self.is_logging = False
+		"""
+			Toggles the logging feature
+		"""
+		self.is_logging = not self.is_logging
 
 	def run(self):
+		"""
+			Main loop for getting items from the queue and printing them
+		"""
 		while self.is_logging or self.queue.qsize() != 0:
 			fle = open(self.file_, 'a')
 			event = self.queue.get()
