e7d43170649862d44d0164031a31842f45328af6
ewright393 <exw4938@g.rit.edu>
2018-04-08 17:13:09 -0400

    Fixed logger to run as daemon and open file for each log line

--- bot/logger.py
+++ bot/logger.py
@@ -3,20 +3,21 @@ from time import strftime
 from queue import Queue
 
 class Logger(Thread):
-	def __init__(self):
-		super.__init__(self)
-		self.file_ = open(strftime("%m.%d.%Y", 'w')
+	def __init__(self, daemon=True):
+		Thread.__init__(self, daemon=True)
+		self.file_ = "log/" + strftime("%m.%d.%Y") + ".lg"
 		self.queue = Queue()
-		self.logging = True
+		self.is_logging = True
 	
 	def log_event(self, event:str):
 		self.queue.put(event)
 	
 	def stop_logging(self):
 		self.is_logging = False
-		self.file_.close()
 
 	def run(self):
-		while self.is_logging:
+		while self.is_logging or self.queue.qsize() != 0:
+			fle = open(self.file_, 'a')
 			event = self.queue.get()
-			self.file_.write(event)
+			fle.write(event)
+			fle.close()
